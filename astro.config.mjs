52f29ed4084ee214473a6a684f285288c6d7c0fc
scraed <czhengac@connect.ust.hk>
2025-06-18 11:37:33 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://scraed.github.io/scraedBlog",
+	site: "https://scraed.github.io",
  	base: "/scraedBlog",
 	trailingSlash: "always",
 	integrations: [
