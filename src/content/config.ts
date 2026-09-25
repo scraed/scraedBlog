278aeea9b27fbaaeb7d70f88c3fde01d089994ab
charrywhite <35485042+charrywhite@users.noreply.github.com>
2026-09-25 17:03:37 +0800

    Revert "Add LanPaint project hub"
    This reverts commit 71397891b4adab7adeb856bfae8b63da6fe5d397.

--- src/content/config.ts
+++ src/content/config.ts
@@ -19,12 +19,6 @@ const postsCollection = defineCollection({
 		nextSlug: z.string().default(""),
 	}),
 });
-
-const specCollection: ReturnType<typeof defineCollection> = defineCollection({
-	schema: z.object({}),
-});
-
 export const collections = {
 	posts: postsCollection,
-	spec: specCollection,
 };
